36241f7f1c5ab7184f6ee23df630919adca43783
aditya.bhuyan71@gmail.com <manson123>
2018-04-26 19:51:17 +0530

    FinalCucumberFramework

---
+++ src/test/java/stepdefinition/ProductAdditionWithAddOnsSteps.java
@@ -0,0 +1,105 @@
+package stepdefinition;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ProductAdditionWithAddOnsSteps {
+
+    @When("^user has logged into CPQ$")
+    public void userLoginCPQ()throws Throwable{
+
+    }
+
+    @And("^user clicks add button$")
+    public void userClicksAddButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
+    @Then("^he selects \"([^\"]*)\" on the product page$")
+    public void heSelectsOnTheProductPage(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he is on the config page$")
+    public void heIsOnTheConfigPage() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^he enters the \"([^\"]*)\" on the  page$")
+    public void heEntersTheOnThePage(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he clicks on check connectivity button$")
+    public void heClicksOnCheckConnectivityButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he should see (\\d+) tabs of site address, site details and features$")
+    public void heShouldSeeTabsOfSiteAddressSiteDetailsAndFeatures(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he moves to site details$")
+    public void heMovesToSiteDetails() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^he chooses \"([^\"]*)\"$")
+    public void heChooses(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he should see base price table$")
+    public void heShouldSeeBasePriceTable() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^two values should be populated against base price$")
+    public void twoValuesShouldBePopulatedAgainstBasePrice() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he click on features$")
+    public void heClickOnFeatures() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^chooses OBH$")
+    public void choosesOBH() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he should see base price and obh in the table below$")
+    public void heShouldSeeBasePriceAndObhInTheTableBelow() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he clicks save to quote$")
+    public void heClicksSaveToQuote() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^he should be in CPQ$")
+    public void heShouldBeInCPQ() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
